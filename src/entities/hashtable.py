211ccb080a8e0e2a7d1dcc8b97c8ae943a0d46ca
d4daveshep <david@daveshep.net.nz>
2022-07-16 10:10:53 +1200

    so far added init, len, my_index, setitem, getitem, delitem, contains and all tested with pytest

--- src/entities/hashtable.py
+++ src/entities/hashtable.py
@@ -5,3 +5,30 @@ class HashTable:
 
     def __len__(self):
         return self.__capacity
+
+    def __my_index(self, key):
+        return hash(key) % len(self)
+
+    def __setitem__(self, key, value):
+        index = self.__my_index(key)
+        self.values[index] = value
+
+    def __getitem__(self, key):
+        index = self.__my_index(key)
+        value = self.values[index]
+        if value is None:
+            raise KeyError(key)
+        return value
+
+    def __delitem__(self, key):
+        index = self.__my_index(key)
+        self.values[index] = None
+
+    def __contains__(self, key):
+        index = self.__my_index(key)
+        try:
+            self[key]
+        except KeyError:
+            return False
+        else:
+            return True
--- src/tests/test_hashtable.py
+++ src/tests/test_hashtable.py
@@ -1,3 +1,5 @@
+import pytest
+
 from entities.hashtable import HashTable
 
 
@@ -18,6 +20,32 @@ def test_should_create_empty_value_slots():
     # Then
     assert actual_values == expected_values
 
+@pytest.fixture
+def sample_hashtable_1():
+    sample_data = HashTable(capacity=100)
+    sample_data["hola"] = "hello"
+    sample_data[98.6] = 37
+    sample_data[False] = True
+    return sample_data
 
+def test_should_insert_key_value_pairs(sample_hashtable_1):
+    assert sample_hashtable_1["hola"] == "hello"
+    assert sample_hashtable_1[98.6] == 37
+    assert sample_hashtable_1[False] == True
 
+def test_should_raise_error_on_missing_key(sample_hashtable_1):
+    with pytest.raises(KeyError) as exception_info:
+        sample_hashtable_1["missing_key"]
+    assert exception_info.value.args[0] == "missing_key"
 
+def test_should_raise_error_on_get_deleted_key(sample_hashtable_1):
+    del sample_hashtable_1["hola"]
+    with pytest.raises(KeyError) as exception_info:
+        sample_hashtable_1["hola"]
+    assert exception_info.value.args[0] == "hola"
+
+def test_should_find_key(sample_hashtable_1):
+    assert "hola" in sample_hashtable_1
+
+def test_should_not_find_key(sample_hashtable_1):
+    assert "missing_key" not in sample_hashtable_1
