6e709c399e78bf013e2821e04cc5cc75587e7726
d4daveshep <david@daveshep.net.nz>
2022-07-16 10:34:51 +1200

    added get() and error on get deleted key

--- src/entities/hashtable.py
+++ src/entities/hashtable.py
@@ -6,29 +6,36 @@ class HashTable:
     def __len__(self):
         return self.__capacity
 
-    def __my_index(self, key):
+    def _index(self, key):
         return hash(key) % len(self)
 
     def __setitem__(self, key, value):
-        index = self.__my_index(key)
+        index = self._index(key)
         self.values[index] = value
 
     def __getitem__(self, key):
-        index = self.__my_index(key)
+        index = self._index(key)
         value = self.values[index]
         if value is None:
             raise KeyError(key)
         return value
 
     def __delitem__(self, key):
-        index = self.__my_index(key)
-        self.values[index] = None
+        if key in self:
+            self[key] = None
+        else:
+            raise KeyError(key)
 
     def __contains__(self, key):
-        index = self.__my_index(key)
         try:
             self[key]
         except KeyError:
             return False
         else:
             return True
+
+    def get(self, key, default=None):
+        try:
+            return self[key]
+        except KeyError:
+            return default
